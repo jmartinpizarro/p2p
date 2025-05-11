265d1c114c6bc3a15cf2948112569ca104931057
tito <alb.pascau@gmail.com>
2025-05-11 23:19:57 +0200

    List_users working

--- src/client/utils/utils.py
+++ src/client/utils/utils.py
@@ -4,17 +4,12 @@ def send_string(sock: socket.socket, s: str) -> None:
     sock.sendall(s.encode('utf-8') + b'\0')
 
 def recv_string(sock: socket.socket) -> str:
-    print("Empezando a recibir string")
     data = bytearray()
     while True:
-        print("Byte recibiendo")
         byte = sock.recv(1)
-        print(f"Byte recibido:{byte}")
         if not byte:
-            print("Conexión cerrada en recv_string")
             raise ConnectionError("Conexión cerrada inesperadamente")
         if byte == b'\0':
-            print("rompiendo")
             break
         data += byte
     return data.decode('utf-8')
